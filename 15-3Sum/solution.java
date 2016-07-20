e9276664ecb949c9467982b0343129ef1da6242d
Shipu Wang <shipu.wsp@gmail.com>
2016-07-20 18:12:57 +0800

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -10,28 +10,32 @@ public class Solution {
 			
 		Arrays.sort(nums);
 		
-        for(int i = 0;i<nums.length-2;i++){
-            int lp = i + 1;
-            int rp = nums.length - 1;
-            while (lp < rp){
-                if ( nums[lp] + nums[rp] == 0 - nums[i]){
-                    ArrayList<Integer> list = new ArrayList<Integer>();
-                    list.add(nums[i]);
-					list.add(nums[lp]);
-					list.add(nums[rp]);
-					//res.add(list);
-					lp ++;
-					rp --;
-					if (!res.contains(list))
-						res.add(list);
-				// 	while (lp < rp && nums[rp] == nums[rp + 1])//remove duplicate triplets
-				// 			rp--;
-				// 	while (lp < rp && nums[lp] == nums[lp - 1])
-				//			lp++;
-                }else if(nums[lp] + nums[rp] < 0 - nums[i]){
-                    lp ++;
-                }else{
-                    rp --;
+        for(int i = 0;i<nums.length - 2;i++){
+            if (i == 0 || nums[i] != nums[i - 1]) {//*remove duplicate triplets!
+                int lp = i + 1;
+                int rp = nums.length - 1;
+                while (lp < rp){
+                    if ( nums[lp] + nums[rp] ==  - nums[i]){
+                        ArrayList<Integer> list = new ArrayList<Integer>();
+                        list.add(nums[i]);
+    					list.add(nums[lp]);
+    					list.add(nums[rp]);
+    					res.add(list);
+    					lp ++;
+    					rp --;
+    
+    					while (lp < rp && nums[rp] == nums[rp + 1]){//remove duplicate triplets
+    							rp--;
+    					}
+    					while (lp < rp && nums[lp] == nums[lp - 1]){
+    							lp++;
+    					}
+                    }else if(nums[lp] + nums[rp] < 0 - nums[i]){
+                        lp ++;
+                    }else{
+                        rp --;
+                    }
+                    
                 }
                 
             }

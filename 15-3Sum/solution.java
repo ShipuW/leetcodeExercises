8b82480bfdd79634d66fb3223041629eb4b51905
Shipu Wang <shipu.wsp@gmail.com>
2016-07-20 17:58:29 +0800

    [15. 3Sum][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 15-3Sum/solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Solution {
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+		if(nums.length<3)
+			return res;
+			
+		Arrays.sort(nums);
+		
+        for(int i = 0;i<nums.length-2;i++){
+            int lp = i + 1;
+            int rp = nums.length - 1;
+            while (lp < rp){
+                if ( nums[lp] + nums[rp] == 0 - nums[i]){
+                    ArrayList<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[i]);
+					list.add(nums[lp]);
+					list.add(nums[rp]);
+					//res.add(list);
+					lp ++;
+					rp --;
+					if (!res.contains(list))
+						res.add(list);
+				// 	while (lp < rp && nums[rp] == nums[rp + 1])//remove duplicate triplets
+				// 			rp--;
+				// 	while (lp < rp && nums[lp] == nums[lp - 1])
+				//			lp++;
+                }else if(nums[lp] + nums[rp] < 0 - nums[i]){
+                    lp ++;
+                }else{
+                    rp --;
+                }
+                
+            }
+        }
+        return res;
+    }
+
+    
+}

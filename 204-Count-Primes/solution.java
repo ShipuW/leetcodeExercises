b3babc5a15ee46aea4b4ccfeee664499fc4716a0
Shipu Wang <shipu.wsp@gmail.com>
2016-07-21 16:54:55 +0800

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -3,7 +3,7 @@ public class Solution {
 
         boolean[] isPrime=new boolean[n];
 		Arrays.fill(isPrime, true);
-        for(int i = 2; i < n; i ++){
+        /*for(int i = 2; i < n; i ++){
             for(int j = 2; j * j <= i; j++){
                 if(isPrime[j]){
                     if(i % j == 0){
@@ -12,7 +12,14 @@ public class Solution {
                     }
                 }
             }
-        }
+        }*/
+        for (int i = 2; i < Math.sqrt(n); i++) {
+			if (isPrime[i]) {
+				for (int j = 2 * i; j < n; j += i)
+					isPrime[j] = false;
+			}
+		}
+
         int res = 0;
         for(int i = 2; i < n; i++){
             res = isPrime[i] == true ? res+1 : res;

ab85a88f610b35293216380af2b8b4b68c6875ee
Shipu Wang <shipu.wsp@gmail.com>
2016-07-23 23:29:23 +0800

    [29. Divide Two Integers][Accepted]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,21 +1,50 @@
 public class Solution {
-    //Error case: -2147483648
+    //TLE case: -2147483648
     // -1
+    // use << to speed up
     public int divide(int dividend, int divisor) {
-        if (divisor == 0)return Integer.MAX_VALUE;
         
-        long absa = Math.abs((long)dividend);
-        long absb = Math.abs((long)divisor);
-        long res = 0;
-        long sum = 0;
-        while (sum <= absa){
-            sum += absb;
-            res ++;
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        
+        long ret = 0;
+        //从小到大减
+        while (a >= b) {
+            for (long tmp = b, cnt = 1; a >= tmp; tmp <<= 1, cnt <<= 1) {
+                ret += cnt;
+                a -= tmp;
+            }
         }
-        res--;
-        if ((dividend < 0 && divisor < 0) || (dividend >= 0 && divisor >= 0))
-            return (int) res;
-        else 
-            return -(int) res;
+        //从大到小减
+        /*
+        while (true) {
+            if ((divisorL << move) > dividendL) { // found the highest digit
+                break;
+            }
+ 
+            move++;
+ 
+        } // while
+ 
+        long temp = dividendL;
+        for (int i = move - 1; i >= 0; i--) {
+            long newDivisor = divisorL << i;
+            if (temp - newDivisor < 0)
+                continue;
+ 
+            temp -= newDivisor;
+            if (i == 0)
+                res += 1;
+            else
+                res += 2 << (i - 1);
+        }
+        */
+        
+        ret = (((dividend ^ divisor) >> 31) & 1) == 1 ? -ret: ret;//deal with out of range case
+        if (ret > Integer.MAX_VALUE || ret < Integer.MIN_VALUE){
+            return Integer.MAX_VALUE;
+        }
+            
+        return (int)ret;
     }
 }

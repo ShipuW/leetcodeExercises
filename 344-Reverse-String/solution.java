6703b2c4dfc380440d487b3f52d8ab4e6004446a
Shipu Wang <shipu.wsp@gmail.com>
2016-07-29 15:50:05 +0800

    [344. Reverse String][Accepted]committed by LeetCode Extension

---
+++ 344-Reverse-String/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String reverseString(String s) {
+        StringBuilder result = new StringBuilder("");
+        for (int i = s.length(); i > 0; i--){
+            result.append(s.charAt(i-1));
+        }
+        return result.toString();
+    }
+}

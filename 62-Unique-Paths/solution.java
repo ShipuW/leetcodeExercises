7f1f907195ffb11bf5a765f53a0a0af8e7d4a56f
Shipu Wang <shipu.wsp@gmail.com>
2016-09-18 00:20:49 -0700

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,8 +1,18 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        if(m==1&&n==1) return 1;
-        else if(m == 1) return uniquePaths(m,n-1);
-        else if(n == 1) return uniquePaths(m-1,n);
-        else return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+        int[][] res = new int[m][n];
+        
+        for(int i = 0; i < m; i++){
+            res[i][n-1]=1;
+        }
+        for(int i = 0; i < n; i++){
+            res[m-1][i]=1;
+        }
+        for(int i = m-2; i>=0; i--){
+            for(int j = n-2; j>=0; j--){
+                res[i][j] = res[i+1][j]+res[i][j+1];
+            } 
+        }
+        return res[0][0];
     }
 }

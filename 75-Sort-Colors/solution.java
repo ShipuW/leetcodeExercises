ba7a1e38a65245adbab169968eb7770cde34b0dc
Shipu Wang <shipu.wsp@gmail.com>
2016-09-18 23:14:19 -0700

    [75. Sort Colors][Accepted]committed by LeetCode Extension

---
+++ 75-Sort-Colors/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        int redpt = 0, bluept = nums.length - 1;
+		int i = 0;
+
+		// stop looping when current >= bluept
+		while (i <= bluept) {
+			// if color is RED, move to the front
+			if (nums[i] == 0) {
+				swap(nums, redpt, i);//always swap with the first one
+				redpt++;
+				i++;
+				continue;
+			} else if (nums[i] == 2) { // if color is BLUE, move to the end
+				swap(nums, bluept, i);
+				bluept--;
+				continue;
+			} else {
+				i++; // if color is WHITE, don't move
+			}
+		}
+    }
+    public void swap(int[] A, int from, int to) {
+		int tmp = A[from];
+		A[from] = A[to];
+		A[to] = tmp;
+	}
+}

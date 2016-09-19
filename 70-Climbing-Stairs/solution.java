5d658d51be064f4cc616db1786da8644de8c5683
Shipu Wang <shipu.wsp@gmail.com>
2016-09-18 17:03:43 -0700

    [70. Climbing Stairs][Accepted]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.java
+++ 70-Climbing-Stairs/solution.java
@@ -1,7 +1,13 @@
 public class Solution {
     public int climbStairs(int n) {
-        if(n==1)return 1;
-        if(n==2)return 2;
-        return climbStairs(n-1) + climbStairs(n-2);
+        if(n == 1)return 1;
+        int[] res = new int[n];
+        res[n-1] = 1;
+        res[n-2] = 2;
+        for(int i = n - 3; i>=0; i--){
+            res[i] = res[i+1]+res[i+2];
+        }
+
+        return res[0];
     }
 }

8370f95639d76840d01afc3017abbb72a10da4b4
Shipu Wang <shipu.wsp@gmail.com>
2016-09-19 21:52:33 -0700

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

---
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i:nums){
+            if(target == i) return true;
+        }
+        return false;
+    }
+
+}
